535462af828092db80325fd612a0af9674711a5a
Rachel Cantor <rachelcantor@gmail.com>
2025-12-10 15:25:00 -0500

    Refactor import statements in router configuration for consistency and clarity
    - Updated import statements in router.tsx to use consistent single quotes for improved readability.
    - Organized imports for better structure and maintainability, aligning with recent code style enhancements.

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,10 +2,10 @@ import { createRouter } from '@tanstack/react-router'
 
 import { rootRoute } from './routes/__root'
 import { baseRoute } from './routes/_base'
-import { baseIndexRoute } from "./routes/_base/"
+import { baseIndexRoute } from './routes/_base/'
 import { notFoundRoute } from './routes/_base/$'
-import { patientsIndexRoute } from "./routes/patients"
-import { patientIdIndexRoute } from "./routes/patients/$patientId"
+import { patientsIndexRoute } from './routes/patients'
+import { patientIdIndexRoute } from './routes/patients/$patientId'
 import { patientIdEditRoute } from './routes/patients/$patientId/edit'
 import { patientIdLayoutRoute } from './routes/patients/$patientId/layout'
 import { patientsNewRoute } from './routes/patients/new'

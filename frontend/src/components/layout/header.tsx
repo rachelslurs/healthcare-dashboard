9db0fa686ecf1d46cceacafb99616ded26394b35
Rachel Cantor <rachelcantor@gmail.com>
2025-12-10 12:45:24 -0500

    Removing feedback

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -14,35 +14,10 @@ import {
 } from '@heroicons/react/16/solid'
 
 export default function Header() {
-  const handleShareFeedback = () => {
-    console.log('Share feedback clicked')
-  }
-
-  const handleSignOut = () => {
-    console.log('Sign out clicked')
-  }
 
   return (
     <Navbar>
-      <NavbarSpacer />
-      <NavbarSection>
-        <Dropdown>
-          <DropdownButton as={NavbarItem}>
-            <UserIcon />
-          </DropdownButton>
-          <DropdownMenu className="min-w-64" anchor="bottom end">
-            <DropdownItem onClick={handleShareFeedback}>
-              <LightBulbIcon />
-              <DropdownLabel>Share feedback</DropdownLabel>
-            </DropdownItem>
-            <DropdownDivider />
-            <DropdownItem onClick={handleSignOut}>
-              <ArrowRightStartOnRectangleIcon />
-              <DropdownLabel>Sign out</DropdownLabel>
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarSection>
+      
     </Navbar>
   )
 }

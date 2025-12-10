90d7ed7ac3007b86723219f6f887ef2f09b89838
Rachel Cantor <rachelcantor@gmail.com>
2025-12-10 15:26:58 -0500

    Refactor import statements in header and router files for improved organization
    - Updated import paths in router.tsx to ensure consistency and clarity.
    - Simplified import statements in header.tsx by removing unused imports, enhancing readability and maintainability.

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -1,18 +1,4 @@
-import {
-  ArrowRightStartOnRectangleIcon,
-  LightBulbIcon,
-  UserIcon,
-} from '@heroicons/react/16/solid'
-
-import {
-  Dropdown,
-  DropdownButton,
-  DropdownDivider,
-  DropdownItem,
-  DropdownLabel,
-  DropdownMenu,
-} from '../ui/dropdown'
-import { Navbar, NavbarItem, NavbarSection, NavbarSpacer } from '../ui/navbar'
+import { Navbar } from '../ui/navbar'
 
 export default function Header() {
 

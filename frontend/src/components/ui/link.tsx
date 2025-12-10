e43b307e76c09b229e877fe3c949f534925a3882
Rachel Cantor <rachelcantor@gmail.com>
2025-12-09 23:42:31 -0500

    Refactor Link component to integrate TanStack Router
    - Updated the Link component to utilize TanStack Router for internal routing while maintaining support for external links.
    - Enhanced documentation within the component to clarify usage and integration with Catalyst UI.
    - Improved type handling by omitting the 'href' prop from standard anchor attributes.

--- frontend/src/components/ui/link.tsx
+++ frontend/src/components/ui/link.tsx
@@ -1,21 +1,49 @@
-/**
- * TODO: Update this component to use your client-side framework's link
- * component. We've provided examples of how to do this for Next.js, Remix, and
- * Inertia.js in the Catalyst documentation:
- *
- * https://catalyst.tailwindui.com/docs#client-side-router-integration
- */
-
 import * as Headless from '@headlessui/react'
+import { Link as TanStackLink } from '@tanstack/react-router'
 import React, { forwardRef } from 'react'
 
+/**
+ * Link component that integrates TanStack Router with Catalyst UI.
+ * 
+ * For internal routes, use the `href` prop and it will be converted to TanStack Router's `to` prop.
+ * For external links (http://, https://, mailto:, tel:), it will use a regular anchor tag.
+ * 
+ * @see https://catalyst.tailwindui.com/docs#client-side-router-integration
+ * @see https://tanstack.com/router/latest/docs/framework/react/guide/link-options
+ */
 export const Link = forwardRef(function Link(
-  props: { href: string } & React.ComponentPropsWithoutRef<'a'>,
+  props: { href: string } & Omit<React.ComponentPropsWithoutRef<'a'>, 'href'>,
   ref: React.ForwardedRef<HTMLAnchorElement>
 ) {
+  const { href, ...restProps } = props
+
+  // Check if it's an external link
+  const isExternal =
+    href.startsWith('http://') ||
+    href.startsWith('https://') ||
+    href.startsWith('mailto:') ||
+    href.startsWith('tel:') ||
+    href.startsWith('#')
+
+  if (isExternal) {
+    // Use regular anchor tag for external links
+    return (
+      <Headless.DataInteractive>
+        <a {...restProps} href={href} ref={ref} />
+      </Headless.DataInteractive>
+    )
+  }
+
+  // Use TanStack Router Link for internal routes
+  // Type assertion needed because TanStack Router's `to` prop is strictly typed
+  // but we're accepting dynamic href strings from Catalyst components
   return (
     <Headless.DataInteractive>
-      <a {...props} ref={ref} />
+      <TanStackLink
+        {...restProps}
+        to={href as any}
+        ref={ref}
+      />
     </Headless.DataInteractive>
   )
 })
